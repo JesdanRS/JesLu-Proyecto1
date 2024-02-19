9c5fc1b76291405ed1c46fcbb50b6ea8b425f8f3
micro <lucivp22@gmail.com>
2024-02-18 23:25:16 -0400

    Clase Tarea y Manejar Tareas base

---
+++ ManejarTareas.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ManejarTareas {
+    public Scanner scanner = new Scanner(System.in);
+    public void agregarTarea(ArrayList<Tarea> listaTareas) {
+
+    }
+
+    public void marcarTareaCompletada(ArrayList<Tarea> listaTareas) {
+
+    }
+
+    public void eliminarTarea(ArrayList<Tarea> listaTareas) {
+
+    }
+
+    public void generarReporte(ArrayList<Tarea> listaTareas) {
+
+    }
+}
